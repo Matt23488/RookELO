912b910fe0c2c05f0f6caa5019fcb360e19aacb8
Matthew Bryant <matt23488@gmail.com>
2017-11-08 10:24:26 -0500

    Calculations complete

--- js/Calculator.js
+++ js/Calculator.js
@@ -13,6 +13,7 @@ function handleNewPlayer(team1, team2) {
     handleTeamUpdate(team1);
     handleTeamUpdate(team2);
     handleGameUpdate(team1, team2);
+    handleGameUpdate(team2, team1);
 }
 
 function handleTeamUpdate(team) {
@@ -23,7 +24,19 @@ function handleTeamUpdate(team) {
     team.transform = Math.pow(10, team.average / 400);
 }
 
-function handleGameUpdate(team1, team2) {
-     team1.expected = team1.transform / (team1.transform + team2.transform);
-    team2.expected = team2.transform / (team2.transform + team1.transform);
+function handleGameUpdate(team, otherTeam) {
+    const player1Score = team.player1 === undefined ? NaN : team.player1.score;
+    const player2Score = team.player2 === undefined ? NaN : team.player2.score;
+
+    team.expected = team.transform / (team.transform + otherTeam.transform);
+    team.ifWon = team.average + 32 * (1 - team.expected);
+    team.ifLoss = team.average + 32 * (0 - team.expected);
+    team.player1ScoreIfWon = Math.ceil((Math.abs(team.average - player1Score) * (1 - team.expected)) + player1Score + 32);
+    team.player2ScoreIfWon = Math.ceil((Math.abs(team.average - player2Score) * (1 - team.expected)) + player2Score + 32);
+    team.player1ScoreIncrease = team.player1ScoreIfWon - player1Score;
+    team.player2ScoreIncrease = team.player2ScoreIfWon - player2Score;
+    team.player1ScoreIfLoss = Math.floor(player1Score - (Math.abs(team.average - player1Score) * (team.expected)) - 32);
+    team.player2ScoreIfLoss = Math.floor(player2Score - (Math.abs(team.average - player2Score) * (team.expected)) - 32);
+    team.player1ScoreDecrease = team.player1ScoreIfLoss - player1Score;
+    team.player2ScoreDecrease = team.player2ScoreIfLoss - player2Score;
 }
--- js/components/Team.js
+++ js/components/Team.js
@@ -13,6 +13,16 @@ export default class TeamComponent extends Component {
         this._average = NaN;
         this._transform = NaN;
         this._expected = NaN;
+        this._ifWon = NaN;
+        this._ifLoss = NaN;
+        this._player1ScoreIncrease = NaN;
+        this._player2ScoreIncrease = NaN;
+        this._player1ScoreIfWon = NaN;
+        this._player2ScoreIfWon = NaN;
+        this._player1ScoreDecrease = NaN;
+        this._player2ScoreDecrease = NaN;
+        this._player1ScoreIfLoss = NaN;
+        this._player2ScoreIfLoss = NaN;
     }
 
     get events() { return this._events; }
@@ -46,7 +56,7 @@ export default class TeamComponent extends Component {
     set average(value) {
         this._average = value;
         this.element.getElementsByClassName("average-display").item(0).innerText = value;
-        getTeamSection(this, "average").classList.toggle("hidden", isNaN(value));
+        getTeamSection(this, "average").classList.toggle("HIDDEN", isNaN(value));
         
     }
     
@@ -54,14 +64,79 @@ export default class TeamComponent extends Component {
     set transform(value) {
         this._transform = value;
         this.element.getElementsByClassName("transform-display").item(0).innerText = value;
-        getTeamSection(this, "transform").classList.toggle("hidden", isNaN(value));
+        getTeamSection(this, "transform").classList.toggle("HIDDEN", isNaN(value));
     }
     
     get expected() { return this._expected; }
     set expected(value) {
         this._expected = value;
         this.element.getElementsByClassName("expected-display").item(0).innerText = value;
-        getTeamSection(this, "expected").classList.toggle("hidden", isNaN(value));
+        getTeamSection(this, "expected").classList.toggle("HIDDEN", isNaN(value));
+    }
+    
+    get ifWon() { return this._ifWon; }
+    set ifWon(value) {
+        this._ifWon = value;
+        this.element.getElementsByClassName("if-won-display").item(0).innerText = value;
+        getTeamSection(this, "hypothetical").classList.toggle("HIDDEN", isNaN(value));
+    }
+
+    get ifLoss() { return this._ifLoss; }
+    set ifLoss(value) {
+        this._ifLoss = value;
+        this.element.getElementsByClassName("if-loss-display").item(0).innerText = value;
+    }
+
+    get player1ScoreIncrease() { return this._player1ScoreIncrease; }
+    set player1ScoreIncrease(value) {
+        this._player1ScoreIncrease = value;
+        this.element.getElementsByClassName("player1-increase-display").item(0).innerText = value;
+        getTeamSection(this, "score-increase").classList.toggle("HIDDEN", isNaN(value));
+    }
+
+    get player2ScoreIncrease() { return this._player2ScoreIncrease; }
+    set player2ScoreIncrease(value) {
+        this._player2ScoreIncrease = value;
+        this.element.getElementsByClassName("player2-increase-display").item(0).innerText = value;
+        getTeamSection(this, "score-increase").classList.toggle("HIDDEN", isNaN(value));
+    }
+
+    get player1ScoreIfWon() { return this._player1ScoreIfWon; }
+    set player1ScoreIfWon(value) {
+        this._player1ScoreIfWon = value;
+        getTeamSection(this, "score-increase").getElementsByClassName("player1-new-score-display").item(0).innerText = value;
+    }
+
+    get player2ScoreIfWon() { return this._player2ScoreIfWon; }
+    set player2ScoreIfWon(value) {
+        this._player2ScoreIfWon = value;
+        getTeamSection(this, "score-increase").getElementsByClassName("player2-new-score-display").item(0).innerText = value;
+    }
+
+    get player1ScoreDecrease() { return this._player1ScoreDecrease; }
+    set player1ScoreDecrease(value) {
+        this._player1ScoreDecrease = value;
+        this.element.getElementsByClassName("player1-decrease-display").item(0).innerText = value;
+        getTeamSection(this, "score-decrease").classList.toggle("HIDDEN", isNaN(value));
+    }
+
+    get player2ScoreDecrease() { return this._player2ScoreDecrease; }
+    set player2ScoreDecrease(value) {
+        this._player2ScoreDecrease = value;
+        this.element.getElementsByClassName("player2-decrease-display").item(0).innerText = value;
+        getTeamSection(this, "score-decrease").classList.toggle("HIDDEN", isNaN(value));
+    }
+
+    get player1ScoreIfLoss() { return this._player1ScoreIfLoss; }
+    set player1ScoreIfLoss(value) {
+        this._player1ScoreIfLoss = value;
+        getTeamSection(this, "score-decrease").getElementsByClassName("player1-new-score-display").item(0).innerText = value;
+    }
+
+    get player2ScoreIfLoss() { return this._player2ScoreIfLoss; }
+    set player2ScoreIfLoss(value) {
+        this._player2ScoreIfLoss = value;
+        getTeamSection(this, "score-decrease").getElementsByClassName("player2-new-score-display").item(0).innerText = value;
     }
 }
 
